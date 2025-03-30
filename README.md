b47c27f905f5c16c7f4c2023c6162dc30cfc9d9c
Z-F-x <benjamin.roedstien@gmail.com>
2025-03-30 21:48:51 +0200

    Fix

--- README.md
+++ README.md
@@ -43,3 +43,110 @@
   },
 
 ```
+
+# vim_default_modern.nvim
+
+A Neovim color scheme inspired by VSCode's modern theme but using Neovim's default colors, with transparent backgrounds by default.
+
+## Features
+
+- Dark and light variants based on Neovim's default color palette
+- Transparent backgrounds throughout the UI
+- Support for LSP, Treesitter, and common plugins
+- Clean, modern look with Neovim's official colors
+
+## Installation
+
+Using [packer.nvim](https://github.com/wbthomason/packer.nvim):
+
+```lua
+use {
+  'username/vim_default_modern.nvim',
+  config = function()
+    require('vim_default_modern').setup({
+      -- Optional configuration
+      cursorline = false,    -- Enable cursorline highlight
+      undercurl = true,      -- Enable undercurls for diagnostics
+      italic_keyword = false, -- Enable italics for keywords
+    })
+    vim.cmd('colorscheme vim_default_modern')
+  end
+}
+```
+
+Using [lazy.nvim](https://github.com/folke/lazy.nvim):
+
+```lua
+{
+  'username/vim_default_modern.nvim',
+  lazy = false,
+  priority = 1000,
+  opts = {
+    -- Optional configuration
+    cursorline = false,    -- Enable cursorline highlight
+    undercurl = true,      -- Enable undercurls for diagnostics
+    italic_keyword = false, -- Enable italics for keywords
+  },
+  config = function(_, opts)
+    require('vim_default_modern').setup(opts)
+    vim.cmd('colorscheme vim_default_modern')
+  end,
+}
+```
+
+## Configuration
+
+The theme comes with these default settings:
+
+```lua
+{
+  cursorline = false,            -- Enable cursorline highlight
+  transparent_background = true, -- Always true (cannot be changed)
+  undercurl = true,              -- Enable undercurls for diagnostics
+  italic_keyword = false,        -- Enable italics for keywords
+}
+```
+
+Note: The `transparent_background` option is always set to `true` for this theme.
+
+## Switching Between Dark and Light Modes
+
+To switch between dark and light variants:
+
+```lua
+-- Dark mode
+vim.opt.background = 'dark'
+vim.cmd('colorscheme vim_default_modern')
+
+-- Light mode
+vim.opt.background = 'light'
+vim.cmd('colorscheme vim_default_modern')
+```
+
+## Colors
+
+This theme uses Neovim's default color palette as defined in the Neovim 0.10+ default colorscheme:
+
+### Dark Theme Colors
+- Background: Transparent
+- Foreground: Light gray
+- Comments: Green
+- Strings: Orange
+- Functions: Yellow
+- Keywords: Blue
+- Types: Teal
+- Variables: Light blue
+
+### Light Theme Colors  
+- Background: Transparent
+- Foreground: Dark gray
+- Comments: Green
+- Strings: Red
+- Functions: Yellow
+- Keywords: Blue
+- Types: Teal
+- Variables: Blue
+
+## Credits
+
+This theme is a transparent adaptation of the VSCode Modern color scheme, using Neovim's default colorscheme values.
--- lua/vim_default_modern/palette.lua
+++ lua/vim_default_modern/palette.lua
@@ -1,143 +1,82 @@
 --- @class VimDefaultModernPalette
 local palette = {
-    dark_01 = '#000000',
-    dark_02 = '#080a0e',
-    dark_03 = '#000000',
-    dark_04 = '#000000',
-    dark_05 = '#000000',
-    dark_06 = '#0000001',
+    -- Neovim default dark theme colors
+    dark_01 = '#0a0b10', -- NvimDarkGray1 - darkest bg
+    dark_02 = '#1c1d23', -- NvimDarkGray2 - darker bg
+    dark_03 = '#2c2e33', -- NvimDarkGray3 - dark bg
+    dark_04 = '#4f5258', -- NvimDarkGray4 - lighter bg
+    dark_05 = '#000000', -- pure black
 
-    grey_01 = '#252526',
-    grey_02 = '#262626',
-    grey_03 = '#282828',
-    grey_04 = '#2a2d2e',
-    grey_05 = '#333333',
-    grey_06 = '#3b3b3b',
-    grey_07 = '#3c3c3c',
-    grey_08 = '#404040',
-    grey_09 = '#424548',
-    grey_10 = '#484848',
-    grey_11 = '#4d4d4d',
-    grey_12 = '#515052',
-    grey_13 = '#515151',
-    grey_14 = '#6e7681',
-    grey_15 = '#707070',
-    grey_16 = '#808080',
-    grey_17 = '#848484',
-    grey_18 = '#858585',
-    grey_19 = '#888888',
-    grey_20 = '#8c8c8c',
-    grey_21 = '#8e8e90',
-    grey_22 = '#939393',
-    grey_23 = '#969696',
-    grey_24 = '#979797',
-    grey_25 = '#999999',
+    -- Grays
+    grey_01 = '#2c2e33', -- NvimDarkGray3
+    grey_02 = '#4f5258', -- NvimDarkGray4
+    grey_03 = '#6b6f7b', -- darker comment/non-text
+    grey_04 = '#8a909c', -- regular comment/non-text
+    grey_05 = '#abb1bf', -- light text/foreground
+    grey_06 = '#c4c6cd', -- NvimLightGray3
+    grey_07 = '#d7dae1', -- NvimLightGray2
+    grey_08 = '#ebeef5', -- NvimLightGray1 - lightest gray
 
-    light_01 = '#a6a6a6',
-    light_02 = '#a9a9a9',
-    light_03 = '#aeafad',
-    light_04 = '#c1c1c1',
-    light_05 = '#c5c5c5',
-    light_06 = '#c6c6c6',
-    light_07 = '#cccccc',
-    light_08 = '#d3d3d3',
-    light_09 = '#d5d5d5',
-    light_10 = '#e5e5e5',
-    light_11 = '#e5efe5',
-    light_12 = '#e8e8e8',
-    light_13 = '#ebebeb',
-    light_14 = '#eeeeee',
-    light_15 = '#f8f8f8',
-    light_16 = '#fafafa',
-    light_17 = '#ffffff',
+    -- Terminal standard colors (dark)
+    term_black = '#0a0b10',    -- NvimDarkGray1
+    term_red = '#5e0009',      -- NvimDarkRed
+    term_green = '#015825',    -- NvimDarkGreen
+    term_yellow = '#6e5600',   -- NvimDarkYellow
+    term_blue = '#005078',     -- NvimDarkBlue
+    term_magenta = '#4c0049',  -- NvimDarkMagenta
+    term_cyan = '#007676',     -- NvimDarkCyan
+    term_white = '#ebeef5',    -- NvimLightGray1
 
-    -- green
-    green_01 = '#007100',
-    green_02 = '#008000',
-    green_03 = '#098658',
-    green_04 = '#2ea043',
-    green_05 = '#329171',
-    green_06 = '#383e2a',
-    green_07 = '#4c5a2c',
-    green_08 = '#4ec9b0',
-    green_09 = '#587c0c',
-    green_10 = '#6a9955',
-    green_11 = '#73c991',
-    green_12 = '#81b88b',
-    green_13 = '#b5cea8',
-    green_14 = '#d7e7b3',
-    green_15 = '#ebf1dd',
+    -- Terminal bright colors (light)
+    term_bright_black = '#4f5258',   -- NvimDarkGray4
+    term_bright_red = '#ffbcb5',     -- NvimLightRed
+    term_bright_green = '#aaedb7',   -- NvimLightGreen
+    term_bright_yellow = '#f4d88c',  -- NvimLightYellow
+    term_bright_blue = '#9fd8ff',    -- NvimLightBlue
+    term_bright_magenta = '#ffc3fa', -- NvimLightMagenta
+    term_bright_cyan = '#83efef',    -- NvimLightCyan
+    term_bright_white = '#ffffff',   -- pure white
 
-    -- yellow
-    yellow_01 = '#795e26',
-    yellow_02 = '#895503',
-    yellow_03 = '#bf8803',
-    yellow_04 = '#c09553',
-    yellow_05 = '#cca700',
-    yellow_06 = '#d7ba7d',
-    yellow_07 = '#dcdcaa',
-    yellow_08 = '#e2c08d',
-    yellow_09 = '#ffcc00',
+    -- Light shades
+    light_01 = '#9b9ea4', -- NvimLightGray4
+    light_02 = '#c4c6cd', -- NvimLightGray3
+    light_03 = '#d7dae1', -- NvimLightGray2
+    light_04 = '#ebeef5', -- NvimLightGray1
+    light_05 = '#f5f5f5', -- almost white
+    light_06 = '#fafafa', -- whiter
+    light_07 = '#ffffff', -- pure white
 
-    -- brown
-    brown_01 = '#613214',
+    -- Syntax colors
+    comment = '#6a9955',    -- green-ish comment color
+    string = '#ce9178',     -- orange-ish string color
+    variable = '#9cdcfe',   -- light blue for variables
+    keyword = '#569cd6',    -- blue for keywords
+    number = '#b5cea8',     -- light green for numbers
+    func = '#dcdcaa',       -- yellow for functions
+    type = '#4ec9b0',       -- teal for types
+    
+    -- Special colors
+    special_char = '#d7ba7d',  -- special characters
+    error = '#f85149',         -- errors
+    warning = '#cca700',       -- warnings
+    info = '#3794ff',          -- information
+    hint = '#75beff',          -- hints
 
-    -- purple
-    purple_01 = '#af00db',
-    purple_02 = '#b180d7',
-    purple_03 = '#c586c0',
+    -- UI colors
+    visual_bg = '#264f78',    -- selection background
+    cursor_line = '#2c2e33',  -- cursor line background
+    fold_bg = '#212e3a',      -- folded text background
+    pmenu_bg = '#333333',     -- popup menu background
+    pmenu_sel = '#04395e',    -- popup menu selection
+    search_bg = '#613214',    -- search highlight background
+    match_paren = '#888888',  -- matching parenthesis highlight
 
-    -- magenta
-    magenta_01 = '#d16d9e',
+    -- Git colors
+    git_add = '#2ea043',      -- git add
+    git_change = '#0078d4',   -- git change
+    git_delete = '#f85149',   -- git delete
 
-    -- blue
-    blue_01 = '#0000ff',
-    blue_02 = '#001080',
-    blue_03 = '#005fb8',
-    blue_04 = '#0066bf',
-    blue_05 = '#0070c1',
-    blue_06 = '#0078d4',
-    blue_07 = '#007acc',
-    blue_08 = '#04395e',
-    blue_09 = '#093e5b',
-    blue_10 = '#0e639c',
-    blue_11 = '#171184',
-    blue_12 = '#1a85ff',
-    blue_13 = '#212e3a',
-    blue_14 = '#264f78',
-    blue_15 = '#267f99',
-    blue_16 = '#2aaaff',
-    blue_17 = '#3375c0',
-    blue_18 = '#3794ff',
-    blue_19 = '#3ba9f2',
-    blue_20 = '#4fc1ff',
-    blue_21 = '#569cd6',
-    blue_22 = '#729db3',
-    blue_23 = '#75beff',
-    blue_24 = '#88bce2',
-    blue_25 = '#9cdcfe',
-    blue_26 = '#add6ff',
-    blue_27 = '#e6f3ff',
-    blue_28 = '#c3d8e6',
-
-    -- orange
-    orange_01 = '#ce9178',
-    orange_02 = '#ee9d28',
-    orange_03 = '#f8c9ab',
-    orange_04 = '#fc6d26',
-
-    -- red
-    red_01 = '#4c1919',
-    red_02 = '#a31515',
-    red_03 = '#ad0707',
-    red_04 = '#c74e39',
-    red_05 = '#d16969',
-    red_06 = '#e51400',
-    red_07 = '#ee0000',
-    red_08 = '#f14c4c',
-    red_09 = '#f85149',
-    red_10 = '#ffcccc',
-
+    -- No background
     none = 'NONE',
 }
 
